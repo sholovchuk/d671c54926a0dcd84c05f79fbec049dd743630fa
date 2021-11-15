fce72d80096bd8b9f23c8cd64b476513ebf91d13
Serhii Holovchuk <serhii.holovchuk@oracle.com>
2021-11-15 16:06:21 +0200

    remove syso

--- src/main/java/com/example/demo/context/WordsRepositoryFactoryBean.java
+++ src/main/java/com/example/demo/context/WordsRepositoryFactoryBean.java
@@ -1,8 +1,12 @@
 package com.example.demo.context;
 
+import java.io.FileNotFoundException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -13,13 +17,23 @@ import com.example.demo.repositories.WordsRepository;
 @Component
 public class WordsRepositoryFactoryBean implements FactoryBean<WordsRepository> {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @Value("${WORDS_FILE_PATH:src/test/resources/fixtures/ordered-words-test-set}")
     private  String filePath;
 
     @Override
     public WordsRepository getObject() throws Exception {
 
-        System.out.println(filePath); // file not found?
+        Path wordFilePath = Paths.get(filePath);
+
+        if(wordFilePath.toFile().exists()) {
+            log.info("Loading file '{}'", filePath);
+        }
+        else {
+            log.error("File '{}' not found", filePath);
+            throw new FileNotFoundException(filePath);
+        }
 
         return new InMemoryPrefixTreeWordsRepository(Files.lines(Paths.get(filePath)));
     }
